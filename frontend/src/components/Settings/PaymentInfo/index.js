9c4bd7401329b1f2211535bf02a959618d0b73ab
Dominique <dominique599@pm.me>
2022-03-24 13:21:40 +0800

    c8/361 removing saved card

--- frontend/src/components/Settings/PaymentInfo/index.js
+++ frontend/src/components/Settings/PaymentInfo/index.js
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useContext } from "react"
+import axios from "axios"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import Button from "@material-ui/core/Button"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
+import CircularProgress from "@material-ui/core/CircularProgress"
 import Switch from "@material-ui/core/Switch"
 import { makeStyles } from "@material-ui/core/styles"
 import Slots from "../Slots"
+import clsx from "clsx"
+import { UserContext, FeedbackContext } from "../../../contexts"
+import { setSnackbar } from "../../../contexts/actions/feedback-actions"
+import { setUser } from "../../../contexts/actions/user-actions"
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js"
 
 import cardIcon from "../../../images/card.svg"
@@ -66,7 +72,9 @@ const useStyles = makeStyles(theme => ({
     height: "2rem",
     marginTop: "-1rem",
   },
-  //   form: {},
+  spinner: {
+    marginLeft: "3rem",
+  },
   //   form: {},
   //   cardNumber: {},
 }))
@@ -84,6 +92,9 @@ function PaymentInfo({
   setCard,
 }) {
   const classes = useStyles({ isCheckout, selectedStep, stepNumber })
+  const { dispatchFeedback } = useContext(FeedbackContext)
+  const { dispatchUser } = useContext(UserContext)
+  const [loading, setLoading] = useState(false)
   const stripe = useStripe()
   const elements = useElements()
   const card =
@@ -95,6 +106,43 @@ function PaymentInfo({
     event.preventDefault()
 
     if (!stripe || !elements) return
+  }
+
+  const removeCard = () => {
+    setLoading(true)
+
+    axios
+      .post(
+        `${process.env.GATSBY_STRAPI_URL}/orders/removecard`,
+        {
+          card: card.last4,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${user.jwt}`,
+          },
+        }
+      )
+      .then(response => {
+        setLoading(false)
+        dispatchUser(
+          setUser({ ...response.data.user, jwt: user.jwt, onboarding: true })
+        )
+        setCardError(true)
+        setCard({ brand: "", last4: "" })
+      })
+      .catch(error => {
+        setLoading(false)
+        console.error(error)
+
+        dispatchFeedback(
+          setSnackbar({
+            status: "error",
+            message:
+              "There was a problem removing your card, please try again.",
+          })
+        )
+      })
   }
 
   const handleCardChange = async event => {
@@ -184,15 +232,30 @@ function PaymentInfo({
           </Typography>
         </Grid>
         {card.last4 && (
-          <Grid item>
-            <Button variant="contained" classes={{ root: classes.deleteCard }}>
-              <Typography
-                variant="h6"
-                classes={{ root: classes.deleteCardText }}
+          <Grid
+            item
+            classes={{
+              root: clsx({
+                [classes.spinner]: loading,
+              }),
+            }}
+          >
+            {loading ? (
+              <CircularProgress color="secondary" />
+            ) : (
+              <Button
+                variant="contained"
+                classes={{ root: classes.deleteCard }}
+                onClick={removeCard}
               >
-                Delete Saved Cards
-              </Typography>
-            </Button>
+                <Typography
+                  variant="h6"
+                  classes={{ root: classes.deleteCardText }}
+                >
+                  Delete Saved Card
+                </Typography>
+              </Button>
+            )}
           </Grid>
         )}
       </Grid>
@@ -216,7 +279,10 @@ function PaymentInfo({
               labelPlacement="start"
               control={
                 <Switch
-                  checked={saveCard}
+                  disabled={user.paymentMethods[slot].last4 !== ""}
+                  checked={
+                    user.paymentMethods[slot].last4 !== "" ? true : saveCard
+                  }
                   onChange={() => setSaveCard(!saveCard)}
                   color="secondary"
                 />
