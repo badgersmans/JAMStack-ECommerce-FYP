73fa7f0296a5e4d2e9bae95b3614066b052d17b1
Dominique <dominique599@pm.me>
2022-03-25 14:28:00 +0800

    c8/370 data grid rows part 2

--- frontend/src/components/Settings/Settings/OrderHistory/index.js
+++ frontend/src/components/Settings/Settings/OrderHistory/index.js
@@ -5,6 +5,11 @@ import Typography from "@material-ui/core/Typography"
 import { DataGrid } from "@material-ui/data-grid"
 import { makeStyles } from "@material-ui/core/styles"
 import { UserContext } from "../../../../contexts"
+import dayjs from "dayjs"
+
+let advancedFormat = require("dayjs/plugin/advancedFormat")
+dayjs.extend(advancedFormat)
+dayjs().format("Do")
 
 const useStyles = makeStyles(theme => ({
   dataGridContainer: {
@@ -23,6 +28,15 @@ const useStyles = makeStyles(theme => ({
     },
     ".MuiDataGrid-root .MuiDataGrid-colCellMoving": {
       "background-color": "transparent",
+    },
+    ".MuiDataGrid-root .MuiDataGrid-cell": {
+      "white-space": "pre-wrap",
+      "max-height": "100% !important",
+      "line-height": "initial !important",
+      padding: "1rem",
+    },
+    ".MuiDataGrid-root .MuiDataGrid-row": {
+      "max-height": "100% !important",
     },
   },
   // dataGridContainer: {},
@@ -60,7 +74,8 @@ function OrderHistory() {
       shipping: `${item.shippingInfo.name}\n${item.shippingAddress.street}\n${item.shippingAddress.city}, ${item.shippingAddress.state}, ${item.shippingAddress.postcode}`,
       order: `#${item.id.slice(item.id.length - 10, item.id.length)}`,
       status: item.status,
-      date: item.createdAt,
+      date: dayjs(item.createdAt).format("Do MMM YYYY h:mma"),
+
       total: item.total,
       id: item.id,
     }))
