72ef7dc4c7c943a077ffb5f202a959fa74b96259
Dominique <dominique599@pm.me>
2022-03-25 15:24:14 +0800

    c8/372 order history navigation

--- frontend/src/components/Settings/Settings/OrderHistory/index.js
+++ frontend/src/components/Settings/Settings/OrderHistory/index.js
@@ -5,9 +5,11 @@ import Typography from "@material-ui/core/Typography"
 import Chip from "@material-ui/core/Chip"
 import { DataGrid } from "@material-ui/data-grid"
 import { makeStyles } from "@material-ui/core/styles"
+import IconButton from "@material-ui/core/IconButton"
 import { UserContext } from "../../../../contexts"
 import dayjs from "dayjs"
 import formatMoney from "../../../../../utils/formatMoney"
+import BackwardsOutline from "../../../../images/BackwardsOutline"
 
 let advancedFormat = require("dayjs/plugin/advancedFormat")
 dayjs.extend(advancedFormat)
@@ -17,6 +19,7 @@ const useStyles = makeStyles(theme => ({
   dataGridContainer: {
     height: "100%",
     width: "100%",
+    // marginBottom: "5rem",
   },
   "@global": {
     ".MuiDataGrid-root .MuiDataGrid-colCellTitle": {
@@ -41,12 +44,13 @@ const useStyles = makeStyles(theme => ({
       "justify-content": "center",
       "align-items": "center",
       "font-weight": 600,
+      "border-bottom": "2px solid white",
     },
     ".MuiDataGrid-root .MuiDataGrid-row": {
       "max-height": "100% !important",
     },
     ".MuiDataGrid-root .MuiDataGrid-footer": {
-      // "margin-top": "-0.5rem",
+      // "margin-top": "-2.5rem",
     },
     ".MuiTablePagination-caption": {
       color: theme.palette.common.WHITE,
@@ -54,18 +58,32 @@ const useStyles = makeStyles(theme => ({
     ".MuiSvgIcon-root": {
       fill: theme.palette.common.WHITE,
     },
+    ".MuiDataGrid-root .MuiDataGrid-columnsContainer": {
+      "background-color": theme.palette.secondary.main,
+      border: "none",
+    },
+    ".MuiDataGrid-root": {
+      border: "none",
+    },
   },
   chipLabel: {
     fontWeight: 600,
   },
-  // dataGridContainer: {},
-  // dataGridContainer: {},
+  header: {
+    height: "8rem",
+    width: "100%",
+    backgroundColor: theme.palette.secondary.main,
+  },
+  iconWrapper: {
+    height: "4rem",
+    width: "4rem",
+  },
   // dataGridContainer: {},
   // dataGridContainer: {},
   // dataGridContainer: {},
 }))
 
-function OrderHistory() {
+function OrderHistory({ setSelectedSetting }) {
   const classes = useStyles()
   const [orders, setOrders] = useState([])
   const { user } = useContext(UserContext)
@@ -128,7 +146,14 @@ function OrderHistory() {
   const rows = createDataRows(orders)
 
   return (
-    <Grid item classes={{ root: classes.dataGridContainer }}>
+    <Grid item container classes={{ root: classes.dataGridContainer }}>
+      <Grid item classes={{ root: classes.header }}>
+        <IconButton onClick={() => setSelectedSetting(null)}>
+          <div className={classes.iconWrapper}>
+            <BackwardsOutline color="#fff" />
+          </div>
+        </IconButton>
+      </Grid>
       <DataGrid rows={rows} columns={columns} pageSize={5} />
     </Grid>
   )
