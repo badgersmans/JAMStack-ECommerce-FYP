c7e87297d370d802314c300cdc7c9421a4cf0497
Dominique <dominique599@pm.me>
2022-03-25 13:17:34 +0800

    c8/367 data grid columns

--- frontend/src/components/Settings/Settings/OrderHistory/index.js
+++ frontend/src/components/Settings/Settings/OrderHistory/index.js
@@ -2,10 +2,23 @@ import React, { useState, useEffect, useContext } from "react"
 import axios from "axios"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
+import { DataGrid } from "@material-ui/data-grid"
 import { makeStyles } from "@material-ui/core/styles"
 import { UserContext } from "../../../../contexts"
 
-const useStyles = makeStyles(theme => ({}))
+const useStyles = makeStyles(theme => ({
+  dataGridContainer: {
+    height: "100%",
+    width: "100%",
+  },
+  // dataGridContainer: {},
+  // dataGridContainer: {},
+  // dataGridContainer: {},
+  // dataGridContainer: {},
+  // dataGridContainer: {},
+  // dataGridContainer: {},
+  // dataGridContainer: {},
+}))
 
 function OrderHistory() {
   const classes = useStyles()
@@ -27,7 +40,27 @@ function OrderHistory() {
       })
   }, [])
 
-  return <div>OrderHistory</div>
+  const columns = [
+    { field: "shipping", headerName: "Shipping", flex: 1 },
+    { field: "order", headerName: "Order", flex: 1 },
+    { field: "status", headerName: "Status", flex: 1 },
+    { field: "date", headerName: "Date", flex: 1 },
+    { field: "total", headerName: "Total", flex: 1 },
+  ]
+
+  const rows = [
+    // { shipping: "Law" },
+    // { order: "Shawn" },
+    // { status: "Shawn" },
+    // { date: "Shawn" },
+    // { total: "Shawn" },
+  ]
+
+  return (
+    <Grid item classes={{ root: classes.dataGridContainer }}>
+      <DataGrid rows={rows} columns={columns} pageSize={1} />
+    </Grid>
+  )
 }
 
 export default OrderHistory
