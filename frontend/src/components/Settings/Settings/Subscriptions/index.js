05422393d519ad8a7548f037edb54ea29eced17d
Dominique <dominique599@pm.me>
2022-04-01 16:39:35 +0800

    c9/444 styling subscription rows

--- frontend/src/components/Settings/Settings/Subscriptions/index.js
+++ frontend/src/components/Settings/Settings/Subscriptions/index.js
@@ -7,10 +7,16 @@ import { UserContext, FeedbackContext } from "../../../../contexts"
 import { setSnackbar } from "../../../../contexts/actions/feedback-actions"
 import SettingsDataGrid from "../../SettingsDataGrid"
 import formatMoney from "../../../../../utils/formatMoney"
+import QuantityButton from "../../../ProductList/QuantityButton"
 
 const useStyles = makeStyles(theme => ({
-  // something: {},
-  // something: {},
+  bold: {
+    fontWeight: 600,
+  },
+  productImage: {
+    height: "10rem",
+    width: "10rem",
+  },
   // something: {},
   // something: {},
   // something: {},
@@ -82,28 +88,80 @@ function Subscriptions({ setSelectedSetting }) {
     )
 
   const columns = [
-    { field: "details", headerName: "Details", width: 250, sortable: false },
-    { field: "item", headerName: "Item", width: 250, sortable: false },
-    { field: "quantity", headerName: "Quantity", width: 250, sortable: false },
+    {
+      field: "details",
+      headerName: "Details",
+      width: 350,
+      sortable: false,
+      renderCell: ({ value }) => (
+        <Typography variant="body2" classes={{ root: classes.bold }}>
+          {`${value.shippingInfo.name}`}
+          <br />
+          {`${value.shippingAddress.street}`}
+          <br />
+          {`${value.shippingAddress.city}, ${value.shippingAddress.state}, ${value.shippingAddress.postcode}`}
+        </Typography>
+      ),
+    },
+    {
+      field: "item",
+      headerName: "Item",
+      width: 250,
+      sortable: false,
+      renderCell: ({ value }) => (
+        <Grid container direction="column" alignItems="center">
+          <Grid item>
+            <img
+              src={`${process.env.GATSBY_STRAPI_URL}${value.product_variant.images[0].url}`}
+              alt={value.productName}
+              className={classes.productImage}
+            />
+          </Grid>
+
+          <Grid item>
+            <Typography variant="body2" classes={{ root: classes.bold }}>
+              {value.productName}
+            </Typography>
+          </Grid>
+        </Grid>
+      ),
+    },
+    {
+      field: "quantity",
+      headerName: "Quantity",
+      width: 250,
+      sortable: false,
+      renderCell: ({ value }) => (
+        <QuantityButton
+          stock={[{ quantity: value.product_variant.quantity }]}
+          variants={value.product_variant}
+          selectedVariant={0}
+          name={value.productName}
+          hideCartButton
+          round
+        />
+      ),
+    },
+
     {
       field: "frequency",
       headerName: "Frequency",
       width: 250,
       sortable: false,
     },
-    { field: "next_delivery", headerName: "Next Delivery", width: 250 },
+    { field: "next_delivery", headerName: "Next Order", width: 250 },
     { field: "total", headerName: "Total", width: 250 },
     { field: "", width: 250, sortable: false },
   ]
 
   const rows = createRows(subscriptions)
-  console.log(`rows ->`, rows)
+  //   console.log(`rows ->`, rows)
   return (
     <SettingsDataGrid
       rows={rows}
       columns={columns}
       setSelectedSetting={setSelectedSetting}
-      rowsPerPage={10}
+      rowsPerPage={2}
     />
   )
 }
