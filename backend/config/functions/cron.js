70227160756425fc436ea34abdbbb2e8b885035c
Dominique <dominique599@pm.me>
2022-04-01 13:17:12 +0800

    c9/437 make recurring charges

--- backend/config/functions/cron.js
+++ backend/config/functions/cron.js
@@ -9,23 +9,48 @@
  *
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#cron-tasks
  */
+const dayjs = require("dayjs");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = {
-  /**
-   * Simple example.
-   * Every monday at 1am.
-   */
-  // '0 1 * * 1': () => {
-  //
-  // }
-
   // everyday at 8am
-  // '0 8 * * *': async () => {
+  "0 8 * * *": async () => {
+    const subscriptionsToday = await strapi.services.subscription.find({
+      next_delivery: dayjs().format(),
+    });
 
-  // }
+    await Promise.allSettled(
+      subscriptionsToday.map(async (subscription) => {
+        // get user all payment methods
+        const paymentMethods = await stripe.paymentMethods.list({
+          customer: subscription.user.stripeID,
+          type: "card",
+        });
 
-  // everyday at 8am
-  "*/1 * * * * *": async (date) => {
-    console.log(date);
+        // get the actual card to be charged
+        const paymentMethod = paymentMethods.data.find(
+          (method) => method.card.last4 === subscription.paymentMethod.last4
+        );
+
+        try {
+          const paymentIntent = await stripe.paymentIntents.create({
+            amount: Math.round(subscription.variant.price * 0.14 * 100),
+            currency: "myr",
+            customer: subscription.user.stripeID,
+            payment_method: paymentMethod.id,
+            off_session: true,
+            confirm: true,
+          });
+        } catch (error) {
+          // notify customer that payment failed, ask them to enter new information
+          console.log(error);
+        }
+      })
+    );
   },
+
+  // everyday at 8am
+  // "*/1 * * * * *": async (date) => {
+  // console.log(date);
+  // },
 };
