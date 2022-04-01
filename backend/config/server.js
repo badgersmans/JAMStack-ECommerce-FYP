847144c502ccabd4470ad7a779cc99a3c233c8f0
Dominique <dominique599@pm.me>
2022-04-01 08:15:42 +0800

    unimportant changes

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // url: env("", "https://a3c9-121-122-85-84.ngrok.io"),
+  // url: env("", "https://b03c-121-122-92-179.ngrok.io"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "1f0dcde13c6a0d84a8df10e9df4250c3"),
